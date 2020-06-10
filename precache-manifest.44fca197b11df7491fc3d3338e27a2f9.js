a4e4e7a627656db89f58813cdd37036d8a48a244
Tong Liang <liang_tong@foxmail.com>
2020-06-10 17:06:11 -0400

    Updates

---
+++ precache-manifest.44fca197b11df7491fc3d3338e27a2f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f7e13221b573bb5de87f301553680e",
+    "url": "/BoardGame/index.html"
+  },
+  {
+    "revision": "0ff8dd5787ebdf500c5c",
+    "url": "/BoardGame/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0018efe1927cb5807f20",
+    "url": "/BoardGame/static/css/main.dc9b96b9.chunk.css"
+  },
+  {
+    "revision": "0ff8dd5787ebdf500c5c",
+    "url": "/BoardGame/static/js/2.b18b3d9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BoardGame/static/js/2.b18b3d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0018efe1927cb5807f20",
+    "url": "/BoardGame/static/js/main.2a2d7e9f.chunk.js"
+  },
+  {
+    "revision": "871a2444d35efc59d9dd",
+    "url": "/BoardGame/static/js/runtime-main.12c31b23.js"
+  },
+  {
+    "revision": "f0f60e526093fdb6bcef7352206acc5b",
+    "url": "/BoardGame/static/media/background.f0f60e52.svg"
+  },
+  {
+    "revision": "63929d1f539c52f1142af185ae6cd046",
+    "url": "/BoardGame/static/media/black.63929d1f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BoardGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a34310543b6ee5f9accf42ff383d0d1",
+    "url": "/BoardGame/static/media/white.4a343105.svg"
+  }
+]);
